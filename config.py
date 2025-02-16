cd1739e216aaec940e5781266c4d8f6ef6f21da4
𓆩 𝐒 𝐇 𝐔 𝐁 𝐇 𝐎 𓆪 <shubhendugarai917@gmail.com>
2025-02-16 13:40:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ImperfectXD/Sohinixmusic",
+    "https://github.com/ImperfectXD/untold",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
